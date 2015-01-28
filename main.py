52775f6d24fb1d32a2a9368e893eb8cf43dc9865
Mike Chen <profmikechen@gmail.com>
2015-01-28 14:59:04 -0800

    iOS review times.

--- main.py
+++ main.py
@@ -15,11 +15,36 @@
 # limitations under the License.
 #
 import webapp2
+from google.appengine.api import urlfetch
+import json
+import logging
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+# fetches the iOS/Mac app review times from Kimono labs, and formats it for the Dash dashboard
+class iOSReviewAPI(webapp2.RequestHandler):
+    def get(self):
+        url = "https://www.kimonolabs.com/api/65ab92xy?apikey=6897a975dea2a52bfae633535c25804f"
+        result = urlfetch.fetch(url)
+        logging.info(result)
+        if result.status_code == 200:
+            js = json.loads(result.content)
+            #self.response.write(js)
+            days = int(js['results']['App Review Times'][0]['iOS'])
+            self.response.write(json.dumps({'value': days, 'formatted': '%i days' % days, 'start': '1', 'end': '15'}))
+        else:
+            self.response('an error occurred')
+
+class AppleStore(webapp2.RequestHandler):
+    def get(self):
+        url = 'http://store.apple.com/us'
+        result = urlfetch.fetch(url)
+        self.response.write(result.status_code)
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/ios-review', iOSReviewAPI),
+    ('/apple-store', AppleStore),
 ], debug=True)
