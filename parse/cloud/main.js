1ba8b40847568fc95a1355d49cbd10bc6024e6db
bblurock <llwbenson@gmail.com>
2015-09-15 15:38:37 +0800

    Change API from "user/show" to "users/lookup"

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -38,19 +38,22 @@ Parse.Cloud.job("twitterFeed", function(request, status) {
 
     var jobs = {
         getUserInfo: function(screenNames){
-
-            var tableName = "user_status";
-
-            // This promise collection make sure we traverse every 'screen_name' successfully
-            var apiCallsPromises = new Array();
+            var tableName = "user_status_dev";
 
             // This promise is for the current method
             var promise = new Parse.Promise();
 
+            var users = new Array();
+
+            var lookupUsers = new Array();
+
+            var queryPromises = new Array();
+
+            // Filter out the screenNames' that already been processed
             _.each(screenNames, function(screenName){
 
-                // Promise for the current 'screen_name'
-                var apiCallPromise = new Parse.Promise();
+                // This promise is for the current method
+                var queryPromise = new Parse.Promise();
 
                 // set today starting at 00:00:00 
                 var today = new Date();
@@ -64,114 +67,22 @@ Parse.Cloud.job("twitterFeed", function(request, status) {
                 query.limit(1);
 
                 query.find().then(function(result){
-
+                    
                     // Job already done today.
-                    if (result.length !== 0) {
-                        return;
+                    if (result.length === 0) {
+                        users.push(screenName);
                     }
 
-                    var urlLink = "https://api.twitter.com/1.1/users/show.json?screen_name=" + screenName;
-
-                    var message = {
-                        method: "GET",
-                        action: urlLink,
-                        parameters: params
-                    };
-
-                    oauth.SignatureMethod.sign(message, accessor);
-
-                    var normPar = oauth.SignatureMethod.normalizeParameters(message.parameters);
-                    var baseString = oauth.SignatureMethod.getBaseString(message);
-                    var sig = oauth.getParameter(message.parameters, "oauth_signature") + "=";
-                    var encodedSig = oauth.percentEncode(sig);
-
-                    Parse.Cloud.httpRequest({
-                        method: "GET",
-                        url: urlLink,
-                        headers: {
-                           Authorization: 'OAuth oauth_consumer_key="' + oauth_consumer_key + '", oauth_nonce=' + nonce + ', oauth_signature=' + encodedSig + ', oauth_signature_method="HMAC-SHA1", oauth_timestamp=' + timestamp + ',oauth_token="'+oauth_token+'", oauth_version="1.0"'
-                        },
-                        success: function(httpResponse) { 
-                 
-                            var data = JSON.parse(httpResponse.text);
-
-                            var usersClass = Parse.Object.extend(tableName),
-                                user = new usersClass();
-
-                            user.set("user_id", data.id);
-                            user.set("screen_name", data.screen_name.toLowerCase());
-                            user.set("following", data.friends_count);
-                            user.set("followers", data.followers_count);
-                            user.set("favorites", data.favourites_count);
-
-                            user.save(null, {
-                                success: function(objs) {
-
-                                    apiCallPromise.resolve();
-                             
-                                },
-                                error: function(error) {
-                                    console.log(error);
-                                    apiCallPromise.reject(error.message);
-                                }
-                            });
-
-                        },
-                        error: function(error) {
-                            console.log(error);
-                            apiCallPromise.reject(error.message);
-                        }
-                    });
-            
-                    apiCallsPromises.push(apiCallPromise);
-
+                    queryPromise.resolve();
                 });
 
+                queryPromises.push(queryPromise);
             });
 
-            Parse.Promise.when(apiCallsPromises).then(function(){
+            // Perform Twitter Api call: user/lookup.json
+            Parse.Promise.when(queryPromises).then(function(){
 
-                promise.resolve();
-
-            }, function(error){
-
-                promise.reject(error.message);
-
-            });
-
-            return promise;
-
-        },
-
-        getTweets: function(screenName){
-
-            var promise = new Parse.Promise();
-
-            var Tweets = Parse.Object.extend("Tweets");
-
-            var query = new Parse.Query(Tweets);
-
-            query.equalTo("screen_name", screenName);
-
-            query.descending("id_str")
-
-            query.limit(1);
-
-            query.find().then(function(results){
-
-                if(results.length === 0){
-                    // If this is the first time this script has run, then we need don't
-                    // need to have the since_id param
-
-                    var urlLink = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" + screenName + "&exclude_replies=true&include_rts=true&count=100";
-
-                } else {
-
-                    var lastTweetId = results[0].get("id_str");
-
-                    var urlLink = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" + screenName + "&exclude_replies=true&include_rts=true&since_id=" + lastTweetId;
-
-                };
+                var urlLink = "https://api.twitter.com/1.1/users/lookup.json?screen_name=" + users.join();
 
                 var message = {
                     method: "GET",
@@ -190,46 +101,36 @@ Parse.Cloud.job("twitterFeed", function(request, status) {
                     method: "GET",
                     url: urlLink,
                     headers: {
-                       Authorization: 'OAuth oauth_consumer_key="'+oauth_consumer_key+'", oauth_nonce=' + nonce + ', oauth_signature=' + encodedSig + ', oauth_signature_method="HMAC-SHA1", oauth_timestamp=' + timestamp + ',oauth_token="'+oauth_token+'", oauth_version="1.0"'
+                       Authorization: 'OAuth oauth_consumer_key="' + oauth_consumer_key + '", oauth_nonce=' + nonce + ', oauth_signature=' + encodedSig + ', oauth_signature_method="HMAC-SHA1", oauth_timestamp=' + timestamp + ',oauth_token="'+oauth_token+'", oauth_version="1.0"'
                     },
-                    success: function(httpResponse) { 
-             
+                    success: function(httpResponse) {
                         var data = JSON.parse(httpResponse.text);
 
                         var tweets = new Array();
 
                         for (var i = 0; i < data.length; i++) {
+                            var usersClass = Parse.Object.extend(tableName),
+                                user = new usersClass();
 
-                            var Tweets = Parse.Object.extend("Tweets"),
-                                tweet = new Tweets(),
-                                content = data[i];
+                            user.set("user_id", data[i].id);
+                            user.set("screen_name", data[i].screen_name.toLowerCase());
+                            user.set("following", data[i].friends_count);
+                            user.set("followers", data[i].followers_count);
+                            user.set("favorites", data[i].favourites_count);
+                            user.set("listed", data[i].listed_count);
 
-                            tweet.set("text", content.text);
-                            tweet.set("source", content.source);
-                            tweet.set("retweet_count", content.retweet_count);
-                            tweet.set("created_at", content.created_at );
-                            tweet.set("favorite_count", content.favorite_count);
-                            tweet.set("retweeted", content.retweeted);
-                            tweet.set("entities", content.entities);
-                            tweet.set("id_str", content.id_str);
-                            tweet.set("screen_name", screenName);
-                         
-                            tweets.push(tweet);
+                            lookupUsers.push(user);
+                        }
 
-                        };
-
-                        Parse.Object.saveAll(tweets, {
+                        Parse.Object.saveAll(lookupUsers, {
                             success: function(objs) {
-
                                 promise.resolve();
-                         
                             },
                             error: function(error) {
                                 console.log(error);
                                 promise.reject(error.message);
                             }
                         });
-
                     },
                     error: function(error) {
                         console.log(error);
@@ -238,10 +139,9 @@ Parse.Cloud.job("twitterFeed", function(request, status) {
                 });
 
             });
-
+            
             return promise;
-
-        }
+        },
     };
 
     for(key in jobs){
