8b7424c6c076bc77b171dacfbc99f6cc26b35bdf
bblurock <llwbenson@gmail.com>
2015-11-18 10:23:46 +0800

    Replace key info with PATH of RUBY

--- dashing/jobs/twitter.rb
+++ dashing/jobs/twitter.rb
@@ -3,10 +3,10 @@ require 'json'
 require 'pp'
 
 twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = '6IaI7dBybFyO1jkHmAk4b0yzq'
-  config.consumer_secret = 'LJHmSWvJxv3QboKAlz1SFJCTmvH04hQOjB5MD7liR0DIRNBc4z'
-  config.access_token = '498587847-UXzfxjpA7W5rgz8c8sPhrmNtUbYswugPU2ohzUAO'
-  config.access_token_secret = 'BuLLnw2Eu8turGO2S7MagDyCxA8oQU0unSVxVtH85OX4z'
+  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
 
 search_term = URI::encode('@tickleapp')
